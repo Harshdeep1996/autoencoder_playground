c94f95277e9d859dd20ba04f9f2256714a8b9478
Harshdeep <16365803+Harshdeep1996@users.noreply.github.com>
2019-09-06 17:18:24 +0100

    Update some more code, still not complete - a lot left :(

--- stochastic_rnn.py
+++ stochastic_rnn.py
@@ -85,13 +85,73 @@ class TrainableStochasticRNN(StochasticRNN):
             **super_state._asdict() ## Get the params from the StochasticRNNState
         )
 
+    def ta_for_tensor(self, x, dynamic_size=False, clear_after_read=False):
+        # Unstack the inputs and outputs based on sequence lengths which need to be fed into the RNN
+        return tf.TensorArray(
+            x.dtype, tf.shape(x)[0],
+            dynamic_size=dynamic_size,
+            clear_after_read=clear_after_read).unstack(x)
+
+    def encode_all(inputs, encoder):
+      """Encodes a timeseries of inputs with a time independent encoder.
+      Args:
+        inputs: A [time, batch, feature_dimensions] tensor.
+        encoder: A network that takes a [batch, features_dimensions] input and
+          encodes the input.
+      Returns:
+        A [time, batch, encoded_feature_dimensions] output tensor.
+      """
+      input_shape = tf.shape(inputs)
+      num_timesteps, batch_size = input_shape[0], input_shape[1]
+      reshaped_inputs = tf.reshape(inputs, [-1, inputs.shape[-1]])
+      inputs_encoded = encoder(reshaped_inputs)
+      inputs_encoded = tf.reshape(inputs_encoded,
+                                  [num_timesteps, batch_size, encoder.output_size])
+      return inputs_encoded
+
     def set_observations(self, observations, seq_lengths):
         """
         args: observations: 2 tensors of shape (max_seq_len, batch_size, data_size)
             should be inputs and targets.
         args: seq_lengths: tensor of length batch size with all the lengths
         """
+        inputs, targets = observations
+        self.seq_lengths = seq_lengths
+        self.max_len_seq = tf.reduce_max(seq_lengths)
+        self.targets_ta = self.ta_for_tensor(targets)
+        # Encode the targets for the LSTM
+        targets_encoded = self.encode_all(targets, self.data_encoder)
+        self.targets_encoded_ta = self.ta_for_tensor(targets_encoded)
+        # Encode the inputs for the LSTM
+        inputs_encoded = self.encode_all(inputs, self.data_encoder)
+
+        ## Feed it into the RNN
+        ## time major is letting if the inputs are of the form of [max__l]
+        rnn_out, _ = tf.nn.dynammic_rnn(
+            self.rnn_cell, inputs_encoded, time_major=True, dtype=tf.float32, scope='forward_rnn')
+        self.rnn_ta = self.ta_for_tensor(rnn_out)
+
+        # This is for SMOOTHING - learning the inference by reversing the inputs
+        if self.rev_rnn_cell:
+            targets_and_rnn_out = tf.concat([rnn_out, targets_encoded], 2) # Along 2nd axis/data points
+            reversed_input = tf.reverse_sequence(targets_and_rnn_out, seq_lengths, seq_axis=0, batch_axis=1)
+            reverse_rnn_out, _ = tf.nn.dynammic_rnn(
+                self.rev_rnn_cell, reversed_input, time_major=True, dtype=tf.float32, scope='reverse_rnn'
+            )
+            reverse_rnn_out = tf.reverse_sequence(reverse_rnn_out, seq_lengths, seq_axis=0, batch_axis=1)
+            self.reverse_rnn_ta = self.ta_for_tensor(reverse_rnn_out)
+
+    def _filtering_proposal(self, rnn_out, prev_latent_encoded, prior, t):
+        return self._proposal(
+            rnn_out, prev_latent_encoded,
+            self.targets_encoded_ta.read(t), prior_mu=prior.mean())
+
+    def _smoothing_proposal(self, rnn_out, prev_latent_encoded, prior, t):
+        return self._proposal(
+            rnn_out, prev_latent_encoded,
+            smoothing_tensors=[self.reverse_rnn_ta.read(t)]
+            prior_mu=prior.mean())
+
+    def proposal(self):
         pass
-
-
 
